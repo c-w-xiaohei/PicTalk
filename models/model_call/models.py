688f9f5cebb9f122f72ecfcaf7cd8b8ccfff1f1d
c-w-xiaohei <1641233466@qq.com>
2025-01-29 17:50:48 +0800

    添加model_call中的参数校验以及异常处理

--- models/model_call/models.py
+++ models/model_call/models.py
@@ -41,124 +41,204 @@ def call_qwen_finetuned(messages:list,stream:bool = False) -> str | Generator[st
     微调大模型调用函数
     stream:是否使用流式输出
     """
-
-    torch.cuda.empty_cache()
-
-    # 模型绝对路径
-    base_model_path = _get_model(Model.INSTURCT)
-    model_path = CHEKPOINT_PATH
-
-    # 更改基础模型路径的配置
-    config_path = path.join(model_path,"adapter_config.json")
-    with open(config_path, 'r') as file:
-        config = json.load(file)
-    config["base_model_name_or_path"] = base_model_path
-    with open(config_path, 'w') as file:
-        json.dump(config, file, indent=4)
-        
-    # 检查是否有可用的GPU，如果有的话，使用第一个GPU
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    # 加载模型和分词器
-    model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype="auto", device_map={"": device})
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
     
-    print("微调模型加载完成，模型名为： " + model_path)
+    # 验证messages格式
+    if not isinstance(messages, list):
+        raise ValueError("参数类型错误:messages参数必须为list类型")
+    
+    for msg in messages:
+        if not isinstance(msg, dict):
+            raise ValueError("参数类型错误:messages中的每个元素必须为dict类型")
+        if "role" not in msg or "content" not in msg: # 每个值需要是字符串
+            raise ValueError("参数类型错误:messages中的每个元素必须包含'role'和'content'键")
+        if not isinstance(msg["role"], str) or not isinstance(msg["content"], str):
+            raise ValueError("参数类型错误:messages中的'role'和'content'值必须为字符串类型")
+    
+    try:
+        torch.cuda.empty_cache()
 
-    text = tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True,
-        streamer=stream
-    )
-    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+        # 模型绝对路径
+        base_model_path = _get_model(Model.INSTURCT)
+        model_path = CHEKPOINT_PATH
 
-    if not stream:
-        # 直接生成响应
-        generated_ids = model.generate(
-            **model_inputs,
-            max_new_tokens=512
-        )
+        # 更改基础模型路径的配置
+        config_path = path.join(model_path,"adapter_config.json")
+        with open(config_path, 'r') as file:
+            config = json.load(file)
+        config["base_model_name_or_path"] = base_model_path
+        with open(config_path, 'w') as file:
+            json.dump(config, file, indent=4)
+            
+        # 检查是否有可用的GPU，如果有的话，使用第一个GPU
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # 加载模型和分词器
+        model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype="auto", device_map={"": device})
+        tokenizer = AutoTokenizer.from_pretrained(model_path)
         
-        # 提取生成的文本
-        generated_ids = [
-            output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-        ]
-        response =  tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        print("微调模型加载完成，模型名为： " + model_path)
 
-        return response
-    else:
-        # 输出流式响应
-        streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+        text = tokenizer.apply_chat_template(
+            messages,
+            tokenize=False,
+            add_generation_prompt=True,
+            streamer=stream
+        )
+        model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
-        # 在单独的线程中调用.generate()
-        generation_kwargs = dict(model_inputs, streamer=streamer, max_new_tokens=100)
-        thread = Thread(target=model.generate, kwargs=generation_kwargs)
-        thread.start()
+        if not stream:
+            # 直接生成响应
+            generated_ids = model.generate(
+                **model_inputs,
+                max_new_tokens=512
+            )
+            
+            # 提取生成的文本
+            generated_ids = [
+                output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+            ]
+            response =  tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
-        return streamer
+            return response
+        else:
+            # 输出流式响应
+            streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+
+            # 在单独的线程中调用.generate()
+            generation_kwargs = dict(model_inputs, streamer=streamer, max_new_tokens=100)
+            thread = Thread(target=model.generate, kwargs=generation_kwargs)
+            thread.start()
+
+            return streamer
+    except torch.cuda.OutOfMemoryError as e:
+        raise MemoryError(f"model_call 异常: 微调大模型调调用过程显存不足，请检查是否使用了GPU：\n{e}")
+    except Exception as e:
+        raise Exception(f"model_call 异常: 微调大模型调用过程中出现异常：\n{e}")
 
 def call_vl(messages:dict)->str:
     """
     messages 示例格式：
     messages = [
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image",
-                    "image":  "data:image;base64,/9j/..."
-                },
-                {"type": "text", "text": '''使用最简洁的话请描述这张图片的场景。'''},
-            ],
-        }
+  {
+    "role": "system",
+    "content": [
+      {
+        "type": "text",
+        "text": "You are a helpful assistant."
+      }
     ]
+  },
+  {
+    "role": "user",
+    "content": [
+      {
+        "type": "image",
+        "image": "data:image;base64,/9j/..."
+      },
+      {
+        "type": "text",
+        "text": "'使用最简洁的话请描述这张图片的场景。'"
+      }
+    ]
+  },
+  {
+    "role": "assistant",
+    "content": [
+      {
+        "type": "text",
+        "text": "这张图片展示了一座建筑前的雕像，雕像位于一个基座上，背景是一座现代建筑。"
+      }
+    ]
+  },
+  {
+    "role": "user",
+    "content": [
+      {
+        "type": "text",
+        "text": "请描述图片的对象"
+      }
+    ]
+  }...
+]
     """
-    model_dir = _get_model(Model.VL)
-    model = Qwen2VLForConditionalGeneration.from_pretrained(
-      model_dir, torch_dtype="auto", device_map="auto"
-    )
+    # 验证messages格式
+    if not isinstance(messages, list):
+        raise ValueError("参数类型错误:messages参数必须为list类型")
+    for msg in messages:
+        if not isinstance(msg, dict):
+            raise ValueError("参数类型错误:messages中的每个元素必须为dict类型")
+        if "role" not in msg or "content" not in msg:
+            raise ValueError("参数类型错误:messages中的每个元素必须包含'role'和'content'键")
+        if not isinstance(msg["content"], list):
+            raise ValueError("参数类型错误:messages中的'content'值必须为list类型")
+        for item in msg["content"]:
+            if not isinstance(item, dict):
+                raise ValueError("参数类型错误:messages中的'content'列表中的元素必须为dict类型")
+            if "type" not in item:
+                 raise ValueError("参数类型错误:messages中的'content'列表中的元素必须包含'type'键")
+            if item["type"] not in ["image", "text"]:
+                raise ValueError("参数类型错误:messages中的'content'列表中的'type'值必须为'image'或'text'")
+            if item["type"] == "image":
+                if "image" not in item:
+                    raise ValueError("参数类型错误:当'type'为'image'时，必须包含'image'键")
+                if str(item["image"]).startswith("data:image;base64,"):
+                    raise ValueError(f"参数类型错误:当'type'为'image'时，'image'值必须为base64格式的字符串")
+            if item["type"] == "text" and "text" not in item:
+                raise ValueError("参数类型错误:当'type'为'text'时，必须包含'text'键")
+    try:
+            model_dir = _get_model(Model.VL)
+            model = Qwen2VLForConditionalGeneration.from_pretrained(
+            model_dir, torch_dtype="auto", device_map="auto"
+            )
+            # The default range for the number of visual tokens per image in the model is 4-16384. You can set min_pixels and max_pixels according to your needs, such as a token count range of 256-1280, to balance speed and memory usage.
+            min_pixels = 256*28*28
+            max_pixels = 1280*28*28
+            processor = AutoProcessor.from_pretrained(model_dir, min_pixels=min_pixels, max_pixels=max_pixels)
 
-    # The default range for the number of visual tokens per image in the model is 4-16384. You can set min_pixels and max_pixels according to your needs, such as a token count range of 256-1280, to balance speed and memory usage.
-    min_pixels = 256*28*28
-    max_pixels = 1280*28*28
-    processor = AutoProcessor.from_pretrained(model_dir, min_pixels=min_pixels, max_pixels=max_pixels)
 
+            # Preparation for inference
+            text = processor.apply_chat_template(
+                messages, tokenize=False, add_generation_prompt=True
+            )
+            image_inputs, video_inputs = process_vision_info(messages)
+            inputs = processor(
+                text=[text],
+                images=image_inputs,
+                videos=video_inputs,
+                padding=True,
+                return_tensors="pt",
+            )
+            inputs = inputs.to("cuda")
 
-    # Preparation for inference
-    text = processor.apply_chat_template(
-        messages, tokenize=False, add_generation_prompt=True
-    )
-    image_inputs, video_inputs = process_vision_info(messages)
-    inputs = processor(
-        text=[text],
-        images=image_inputs,
-        videos=video_inputs,
-        padding=True,
-        return_tensors="pt",
-    )
-    inputs = inputs.to("cuda")
+            # Inference: Generation of the output
+            generated_ids = model.generate(**inputs, max_new_tokens=256)
+            generated_ids_trimmed = [
+                out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+            ]
+            output_text = processor.batch_decode(
+                generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+            )
+            if output_text is not None:
+                output_text = output_text[0]
 
-    # Inference: Generation of the output
-    generated_ids = model.generate(**inputs, max_new_tokens=256)
-    generated_ids_trimmed = [
-        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-    ]
-    output_text = processor.batch_decode(
-        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-    )
-
-    return output_text
+            return output_text
+    except torch.cuda.OutOfMemoryError as e:
+        raise MemoryError(f"model_call 异常: 视觉模型调调用过程显存不足，请检查是否使用了GPU：{e}")
+    except Exception as e:
+        raise Exception(f"model_call 异常: 视觉模型调用过程中出现异常：{e}")
 
 def call_tts(text:str)->bytes: 
 
-    # 创建一个文本到语音的处理管道，指定任务类型为文本到语音，并使用指定的模型
-    sambert_hifigan_tts = pipeline(task=Tasks.text_to_speech, model=Model.TTS.value)
+    try:
+        # 创建一个文本到语音的处理管道，指定任务类型为文本到语音，并使用指定的模型
+        sambert_hifigan_tts = pipeline(task=Tasks.text_to_speech, model=Model.TTS.value)
 
-    # 使用管道处理输入文本，指定使用的语音风格为 'zhitian_emo'
-    output = sambert_hifigan_tts(input=text, voice='zhitian_emo')
+        # 使用管道处理输入文本，指定使用的语音风格为 'zhitian_emo'
+        output = sambert_hifigan_tts(input=text, voice='zhitian_emo')
 
-    # 从输出中提取生成的 WAV 音频数据
-    wav = output[OutputKeys.OUTPUT_WAV]
+        # 从输出中提取生成的 WAV 音频数据
+        wav = output[OutputKeys.OUTPUT_WAV]
 
-    return wav
+        return wav
+    except Exception as e:
+        raise Exception(f"model_call 异常: 文本到语音模型调用过程中出现异常：{e}")
